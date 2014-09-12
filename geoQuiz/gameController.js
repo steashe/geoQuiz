d9511d0cdbd0bfcac85c2bc72ac60809cddc989b
stephen ashe <stephenashe@hotmail.co.uk>
2014-09-12 17:01:03 +1000

    initial work

--- geoQuiz/gameController.js
+++ geoQuiz/gameController.js
@@ -1,1 +1,77 @@
-﻿
+﻿//have to change alerts to actually doing something
+gameController = {
+    ScoreSheet: {
+        /// <field name="playerLives" type="Number">Player life count</field>
+        playerLives: 3,
+        /// <field name="playerScore" type="Number">Player score</field>
+        playerScore: 0,
+    },
+
+    init: function () {
+        this.setup();
+    },
+
+    setup: function () {
+        $('#newGame').click(function () {
+            gameController.newGame();
+        });
+        
+        $('#test').click(function () {
+            gameController.checkUserChoice(false);
+        });
+
+        $('#test2').click(function () {
+            gameController.checkUserChoice(true);
+        });
+    },
+
+    checkUserChoice: function (correct){
+        //then call loadNextData
+        if (correct) {
+            this.increaseScore();
+        } else {
+            this.loseLife();
+        }
+    },
+
+    checkGameOver: function () {
+        if (this.ScoreSheet.playerLives === 0) {
+            alert('game over');
+            //disable options
+            $('#newGame').css('visibility', 'visible');
+        }
+    },
+
+    increaseScore: function () {
+        this.ScoreSheet.playerScore++;
+        $('#score').html('');
+        $('#score').append('Score: ' + this.ScoreSheet.playerScore);
+        //could improved to include some kind of difficulty factor
+    },
+
+    loadNextData: function () {
+        //site to load JSON data from
+        $.getJSON('https://raw.githubusercontent.com/mledoze/countries/master/countries.json',
+        function (result) {
+            alert(result);
+        });
+        //import data from JSON file and place into html
+    },
+
+    loseLife: function () {
+        $('#life' + this.ScoreSheet.playerLives).css('visibility', 'hidden');
+        this.ScoreSheet.playerLives--;
+        this.checkGameOver();
+    },
+
+    newGame: function () {
+        this.ScoreSheet.playerLives = 3;
+        this.ScoreSheet.playerScore = 0;
+        $('#score').html('');
+        $('#score').html('Score: 0');
+        $('#newGame').css('visibility', 'hidden');
+        $('#score').css('visibility', 'visible');
+        $('img').css('visibility', 'visible');
+        this.loadNextData();
+    },
+};
