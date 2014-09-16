8e129b40e676bf8e5680ab188f19943886914fc9
stephen ashe <stephenashe@hotmail.co.uk>
2014-09-16 17:53:24 +1000

    dont know

---
+++ geoQuiz/Country.js
@@ -0,0 +1,31 @@
+﻿//class Player 
+
+// Properties //
+Country.prototype.name = '';
+Country.prototype.capital = '';
+Country.prototype.latitude = '';
+Country.prototype.longitude = '';
+Country.prototype.region = '';
+Country.prototype.subRegion = '';
+Country.prototype.callCode = '';
+
+
+// Constructor
+function Country(name, capital, latitude, longitude, region, subRegion, callCode) {
+    /// <field name="name" type="String">player name</field> 
+    this.name = name;
+
+    this.capital = capital;
+
+    this.latitude = latitude;
+
+    this.longitude = longitude;
+
+    this.region = region;
+
+    this.subRegion = subRegion;
+
+    this.callCode = callCode;
+}
+
+// Methods //
