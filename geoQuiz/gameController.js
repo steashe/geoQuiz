4eb150646b6fc51ef671ba1e7a6a1d0d91fac6ea
stephen ashe <stephenashe@hotmail.co.uk>
2014-09-16 17:30:03 +1000

    updated

--- geoQuiz/gameController.js
+++ geoQuiz/gameController.js
@@ -1,10 +1,17 @@
-﻿//have to change alerts to actually doing something
+﻿/// <reference path="country.js" />
 gameController = {
     ScoreSheet: {
         /// <field name="playerLives" type="Number">Player life count</field>
         playerLives: 3,
         /// <field name="playerScore" type="Number">Player score</field>
         playerScore: 0,
+
+        gameType: ''
+    },
+
+    Data: {
+        allCountries: [],
+        allAnswers: []
     },
 
     init: function () {
@@ -13,15 +20,11 @@ gameController = {
 
     setup: function () {
         $('#newGame').click(function () {
-            gameController.newGame();
-        });
-        
-        $('#test').click(function () {
-            gameController.checkUserChoice(false);
+            gameController.newGame('capital');
         });
 
-        $('#test2').click(function () {
-            gameController.checkUserChoice(true);
+        $('#newGame2').click(function () {
+            gameController.newGame('callCode');
         });
     },
 
@@ -29,16 +32,24 @@ gameController = {
         //then call loadNextData
         if (correct) {
             this.increaseScore();
+            this.reset();
+            this.loadNextCountry();
         } else {
+            this.reset();
             this.loseLife();
-        }
+        }         
     },
 
     checkGameOver: function () {
-        if (this.ScoreSheet.playerLives === 0) {
-            alert('game over');
-            //disable options
+        if (this.ScoreSheet.playerLives === 0 || this.Data.allCountries.length === 0) {
+            alert('Game Over. You scored ' + this.ScoreSheet.playerScore + ' points.');
+            this.reset();
             $('#newGame').css('visibility', 'visible');
+            this.Data.allCountries = [];
+            this.Data.allAnswers = [];
+            $('#score').html('');
+        } else {
+            this.loadNextCountry();
         }
     },
 
@@ -46,16 +57,91 @@ gameController = {
         this.ScoreSheet.playerScore++;
         $('#score').html('');
         $('#score').append('Score: ' + this.ScoreSheet.playerScore);
-        //could improved to include some kind of difficulty factor
     },
 
-    loadNextData: function () {
-        //site to load JSON data from
-        $.getJSON('https://raw.githubusercontent.com/mledoze/countries/master/countries.json',
-        function (result) {
-            alert(result);
+    loadData: function () {
+        $.ajax({
+            type: "get",
+            dataType: "text",
+            async: false,
+            url: "countries2.json",
+
+            success: function (result) {
+                var json = JSON.parse(result);
+                for (var x = 0; x < json.countries.length; x++) {
+                    var name = json.countries[x].name.common;
+                    var capital = json.countries[x].capital;
+                    var latitude = json.countries[x].latlng[0];
+                    var longitude = json.countries[x].latlng[1];
+                    var region = json.countries[x].region;
+                    var subRegion = json.countries[x].subregion;
+                    var callCode = json.countries[x].callingCode;
+
+                    if (gameController.ScoreSheet.gameType === 'capital') {
+                        if (capital != '') {
+                            var nation = new Country(name, capital, latitude, longitude, region, subRegion, callCode);
+                            gameController.Data.allCountries.push(nation);
+                            gameController.Data.allAnswers.push(capital);
+                        }
+                    } else {//delete or find something better and remove all references of callCode and gametype
+                        if (callCode != '') {
+                            var nation = new Country(name, capital, latitude, longitude, region, subRegion, callCode);
+                            gameController.Data.allCountries.push(nation);
+                            gameController.Data.allAnswers.push(callCode);
+                        }
+                    }
+                }
+            },
+
+            error: function (e) {
+                alert('Please Refresh');
+            }
         });
-        //import data from JSON file and place into html
+    },
+
+    loadNextCountry: function () {
+        var answer = Math.floor(Math.random() * (this.Data.allCountries.length));
+        var random = Math.floor(Math.random() * (this.Data.allAnswers.length));
+        var random2 = Math.floor(Math.random() * (this.Data.allAnswers.length));
+        //can still get 2 the same
+        if (random === random2) {
+            random2 = Math.floor(Math.random() * (this.Data.allAnswers.length));
+        }
+
+        if (answer === random) {
+            random = Math.floor(Math.random() * (this.Data.allAnswers.length));
+        }
+
+        if (answer === random2) {
+            random2 = Math.floor(Math.random() * (this.Data.allAnswers.length));
+        }
+
+        $('#country').append(this.Data.allCountries[answer].name);
+
+        if (this.ScoreSheet.gameType === 'capital') {
+            $('#options').append('<button id = "answer">' + this.Data.allCountries[answer].capital + '</button>');
+        } else {
+            $('#options').append('<button id = "answer">' + this.Data.allCountries[answer].callCode + '</button>');
+        }
+
+        $('#options').append('<button class = "option">' + this.Data.allAnswers[random] + '</button>' +
+                             '<button class = "option">' + this.Data.allAnswers[random2] + '</button>');
+
+        var options = document.getElementById('options');
+        for (var i = options.children.length; i >= 0; i--) {
+            options.appendChild(options.children[Math.random() * i | 0]);
+        }
+
+        $('.option').click(function () {
+            gameController.checkUserChoice(false);
+        });
+
+        $('#answer').click(function () {
+            gameController.checkUserChoice(true);
+        });
+
+        //cut the chosen one out
+        this.Data.allCountries.splice(answer, 1);
     },
 
     loseLife: function () {
@@ -64,14 +150,20 @@ gameController = {
         this.checkGameOver();
     },
 
-    newGame: function () {
+    reset: function () {
+        $('#country').html('');
+        $('#options').html('');
+    },
+
+    newGame: function (gameType) {
         this.ScoreSheet.playerLives = 3;
         this.ScoreSheet.playerScore = 0;
-        $('#score').html('');
+        this.ScoreSheet.gameType = gameType;
+
         $('#score').html('Score: 0');
         $('#newGame').css('visibility', 'hidden');
-        $('#score').css('visibility', 'visible');
         $('img').css('visibility', 'visible');
-        this.loadNextData();
+        this.loadData();
+        this.loadNextCountry();
     },
 };
