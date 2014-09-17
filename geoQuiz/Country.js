8d2370b2dbe66febf5c5eaf3dbccc390db42d0b2
stephen ashe <stephenashe@hotmail.co.uk>
2014-09-17 10:23:22 +1000

    comments update

--- geoQuiz/Country.js
+++ geoQuiz/Country.js
@@ -7,25 +7,19 @@ Country.prototype.latitude = '';
 Country.prototype.longitude = '';
 Country.prototype.region = '';
 Country.prototype.subRegion = '';
-Country.prototype.callCode = '';
-
 
 // Constructor
-function Country(name, capital, latitude, longitude, region, subRegion, callCode) {
-    /// <field name="name" type="String">player name</field> 
+function Country(name, capital, latitude, longitude, region, subRegion) {
+    /// <field name="name" type="String">country name</field> 
     this.name = name;
-
+    /// <field name="capital" type="String">country's capital</field> 
     this.capital = capital;
-
+    /// <field name="latitude" type="Number">latitude co-ordinate</field> 
     this.latitude = latitude;
-
+    /// <field name="longitude" type="Number">longitude co-ordinate</field> 
     this.longitude = longitude;
-
+    /// <field name="region" type="String">country's region</field> 
     this.region = region;
-
+    /// <field name="subRegion" type="String">country's sub region</field> 
     this.subRegion = subRegion;
-
-    this.callCode = callCode;
 }
-
-// Methods //
--- geoQuiz/gameController.js
+++ geoQuiz/gameController.js
@@ -5,12 +5,12 @@ gameController = {
         playerLives: 3,
         /// <field name="playerScore" type="Number">Player score</field>
         playerScore: 0,
-
-        gameType: ''
     },
 
     Data: {
+        //stores all country objects
         allCountries: [],
+        //stores all possible answers
         allAnswers: []
     },
 
@@ -19,17 +19,15 @@ gameController = {
     },
 
     setup: function () {
+        //adding event handler
         $('#newGame').click(function () {
-            gameController.newGame('capital');
-        });
-
-        $('#newGame2').click(function () {
-            gameController.newGame('callCode');
+            gameController.newGame();
         });
     },
 
-    checkUserChoice: function (correct){
-        //then call loadNextData
+    checkUserChoice: function (correct) {
+        /// <summary>check the users selected answer</summary>
+        /// <param name="correct" type="Boolean">correct boolean</param>
         if (correct) {
             this.increaseScore();
             this.reset();
@@ -41,12 +39,16 @@ gameController = {
     },
 
     checkGameOver: function () {
+        /// <summary>check if the player is out of lives or finished the game</summary>
         if (this.ScoreSheet.playerLives === 0 || this.Data.allCountries.length === 0) {
             alert('Game Over. You scored ' + this.ScoreSheet.playerScore + ' points.');
             this.reset();
             $('#newGame').css('visibility', 'visible');
+
+            //empty arrays
             this.Data.allCountries = [];
             this.Data.allAnswers = [];
+
             $('#score').html('');
         } else {
             this.loadNextCountry();
@@ -54,19 +56,22 @@ gameController = {
     },
 
     increaseScore: function () {
+        /// <summary>increase the users score by one</summary>
         this.ScoreSheet.playerScore++;
         $('#score').html('');
         $('#score').append('Score: ' + this.ScoreSheet.playerScore);
     },
 
     loadData: function () {
+        /// <summary>load the json data from the file</summary>
         $.ajax({
             type: "get",
             dataType: "text",
             async: false,
-            url: "countries2.json",
+            url: "countries.json",
 
             success: function (result) {
+                //creating JavaScript object of the json data
                 var json = JSON.parse(result);
                 for (var x = 0; x < json.countries.length; x++) {
                     var name = json.countries[x].name.common;
@@ -75,21 +80,12 @@ gameController = {
                     var longitude = json.countries[x].latlng[1];
                     var region = json.countries[x].region;
                     var subRegion = json.countries[x].subregion;
-                    var callCode = json.countries[x].callingCode;
 
-                    if (gameController.ScoreSheet.gameType === 'capital') {
-                        if (capital != '') {
-                            var nation = new Country(name, capital, latitude, longitude, region, subRegion, callCode);
-                            gameController.Data.allCountries.push(nation);
-                            gameController.Data.allAnswers.push(capital);
-                        }
-                    } else {//delete or find something better and remove all references of callCode and gametype
-                        if (callCode != '') {
-                            var nation = new Country(name, capital, latitude, longitude, region, subRegion, callCode);
-                            gameController.Data.allCountries.push(nation);
-                            gameController.Data.allAnswers.push(callCode);
-                        }
-                    }
+                    if (capital != '') {
+                        var nation = new Country(name, capital, latitude, longitude, region, subRegion);
+                        gameController.Data.allCountries.push(nation);
+                        gameController.Data.allAnswers.push(capital);
+                    }   
                 }
             },
 
@@ -100,9 +96,15 @@ gameController = {
     },
 
     loadNextCountry: function () {
+        /// <summary>load the next country</summary>
         var answer = Math.floor(Math.random() * (this.Data.allCountries.length));
         var random = Math.floor(Math.random() * (this.Data.allAnswers.length));
         var random2 = Math.floor(Math.random() * (this.Data.allAnswers.length));
+
+
+
+
+
         //can still get 2 the same
         if (random === random2) {
             random2 = Math.floor(Math.random() * (this.Data.allAnswers.length));
@@ -116,22 +118,25 @@ gameController = {
             random2 = Math.floor(Math.random() * (this.Data.allAnswers.length));
         }
 
+
+
+
+
+
         $('#country').append(this.Data.allCountries[answer].name);
 
-        if (this.ScoreSheet.gameType === 'capital') {
-            $('#options').append('<button id = "answer">' + this.Data.allCountries[answer].capital + '</button>');
-        } else {
-            $('#options').append('<button id = "answer">' + this.Data.allCountries[answer].callCode + '</button>');
-        }
-
-        $('#options').append('<button class = "option">' + this.Data.allAnswers[random] + '</button>' +
+        //create answer buttons
+        $('#options').append('<button id = "answer">' + this.Data.allCountries[answer].capital + '</button>' + 
+                             '<button class = "option">' + this.Data.allAnswers[random] + '</button>' +
                              '<button class = "option">' + this.Data.allAnswers[random2] + '</button>');
 
+        //shuffle answer buttons
         var options = document.getElementById('options');
         for (var i = options.children.length; i >= 0; i--) {
             options.appendChild(options.children[Math.random() * i | 0]);
         }
 
+        //add event handlers to the newly created buttons
         $('.option').click(function () {
             gameController.checkUserChoice(false);
         });
@@ -140,25 +145,27 @@ gameController = {
             gameController.checkUserChoice(true);
         });
 
-        //cut the chosen one out
+        //cut the chosen country out
         this.Data.allCountries.splice(answer, 1);
     },
 
     loseLife: function () {
+        /// <summary>the player loses a life</summary>
         $('#life' + this.ScoreSheet.playerLives).css('visibility', 'hidden');
         this.ScoreSheet.playerLives--;
         this.checkGameOver();
     },
 
     reset: function () {
+        /// <summary>reset the html in specific IDs</summary>
         $('#country').html('');
         $('#options').html('');
     },
 
-    newGame: function (gameType) {
+    newGame: function () {
+        /// <summary>create a new game</summary>
         this.ScoreSheet.playerLives = 3;
         this.ScoreSheet.playerScore = 0;
-        this.ScoreSheet.gameType = gameType;
 
         $('#score').html('Score: 0');
         $('#newGame').css('visibility', 'hidden');
